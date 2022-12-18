10ba3b37273db87b35093e9b513a8af608f6b230
Elias Joostens <elias.joostens@student.ehb.be>
2022-12-18 22:31:17 +0100

    Random Prefix functie toegevoegd

--- HashChallenge.mjs
+++ HashChallenge.mjs
@@ -1,8 +1,16 @@
 import fetch from 'node-fetch'
 import _sodium from "libsodium-wrappers";
 
-function concat(array1, array2){
+function voegSamen(array1, array2){
     return new Uint8Array([...array1, ...array2])
+}
+
+function maakRandomPrefix() {
+    var randomArray = new Uint8Array(4);
+    for (let i = 0; i < 4; i++) {
+        randomArray[i] = Math.floor(Math.random() * 255);
+    }
+    return randomArray;
 }
 
 fetch('https://g5qrhxi4ni.execute-api.eu-west-1.amazonaws.com/Prod/hash',{
@@ -13,22 +21,23 @@ fetch('https://g5qrhxi4ni.execute-api.eu-west-1.amazonaws.com/Prod/hash',{
         _sodium?.ready.then(() => {
             const sodium = _sodium;
             let message = sodium?.from_base64(data.message, sodium?.base64_variants.ORIGINAL)
-            let candidate, byteArray
+            let nieuwePrefix, byteArray
             let teller = 0
-            let result;
+            let hash, base64NieuwePrefix;
             do{
-                candidate = sodium.randombytes_buf(3)
-                byteArray = concat(candidate,message)
+                nieuwePrefix = maakRandomPrefix()
+                    //sodium.randombytes_buf(3)
+                byteArray = voegSamen(nieuwePrefix,message)
                 teller++
-                result = sodium.crypto_generichash(_sodium.crypto_generichash_BYTES, byteArray)
+                hash = sodium.crypto_generichash(_sodium.crypto_generichash_BYTES, byteArray)
                 console.log("Poging" + teller)
-            } while (!(result[0] === 0 && result[1] === 0))
-            candidate = _sodium?.to_base64(candidate, _sodium?.base64_variants.ORIGINAL)
+            } while (!(hash[0] === 0 && hash[1] === 0))
+            base64NieuwePrefix = _sodium?.to_base64(nieuwePrefix, _sodium?.base64_variants.ORIGINAL)
             try {
                 fetch('https://g5qrhxi4ni.execute-api.eu-west-1.amazonaws.com/Prod/hash/' + data.challengeId,
                     {
                         method: 'delete',
-                        body: JSON.stringify({prefix: candidate})
+                        body: JSON.stringify({prefix: base64NieuwePrefix})
                     }).then(response => {
                     console.log(response.status)
                 })
