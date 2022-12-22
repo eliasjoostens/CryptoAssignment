25486e2415dbebb337c511a903934b21dc37553a
Elias Joostens <elias.joostens@student.ehb.be>
2022-12-22 18:36:01 +0100

    ciphertext and key translation

--- DecryptChallenge.mjs
+++ DecryptChallenge.mjs
@@ -12,7 +12,7 @@ fetch('https://g5qrhxi4ni.execute-api.eu-west-1.amazonaws.com/Prod/decrypt',{
                 let nonce = sodium?.from_base64(data.nonce, sodium?.base64_variants.ORIGINAL)
                 let cijfertekst = sodium?.from_base64(data.ciphertext, sodium?.base64_variants.ORIGINAL)
                 let sleutel = sodium?.from_base64(data.key, sodium?.base64_variants.ORIGINAL)
-                const klaartekst = sodium?.crypto_secretbox_open_easy(ciphertext, nonce, key)
+                const klaartekst = sodium?.crypto_secretbox_open_easy(cijfertekst, nonce, sleutel)
                 const base64Klaartekst = sodium.to_base64(klaartekst, sodium?.base64_variants.ORIGINAL)
     //console.log('https://g5qrhxi4ni.execute-api.eu-west-1.amazonaws.com/Prod/decrypt/' + data.challengeId)
                 fetch('https://g5qrhxi4ni.execute-api.eu-west-1.amazonaws.com/Prod/decrypt/' + data.challengeId, {
